b8222ea84428d3d310e630604b7a9c46a9a5ff99
Eldar Zainashev <aeoloth@yandex.ru>
2021-08-31 23:25:23 +0300

    second commit

--- hw2-t3/src/index.js
+++ hw2-t3/src/index.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import App from './App';
+// import reportWebVitals from './reportWebVitals';
 
+function Tick() {
+const watch = <h1>Локальное время {new Date().toLocaleTimeString()}</h1>;
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  watch,
   document.getElementById('root')
 );
+};
 
+setInterval(Tick, 30000);
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
